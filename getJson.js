adf25004b12f3b475287064a9fe9a3c3ea1ac8d3
ciamac-da <ciamac.davoudi@gmail.com>
2020-12-13 12:15:03 +0100

    Verified by Cia v1.0.5  getJSON done

--- getJson.js
+++ getJson.js
@@ -1,7 +1,21 @@
 const getMyUsers = document.getElementById("getUsers");
-
+const userOutput = document.getElementById("output")
 const getUsers = () =>{
     fetch("users.json")
+    .then((res)=>res.json())
+    .then((data)=>{
+        let output = `<h2>Users</h2>`
+        data.forEach( user => {
+            output += `
+            <ul>
+            <li> ID:    ${user.id} </li>
+            <li> Name:  ${user.name} </li>
+            <li> Email: ${user.email} </li>
+            </ul>
+            `;
+        });
+        userOutput.innerHTML = output;
+    })
 }
 
 
