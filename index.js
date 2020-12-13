03d2a52ab22deee64bb62ee4f437c19bf66b6e27
ciamac-da <ciamac.davoudi@gmail.com>
2020-12-13 10:59:04 +0100

    Verified by Cia v1.0.3 get text done

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const getMyText = document.getElementById("getText")
-
-const getText =()=>{
-    console.log("123");
+const output = document.getElementById("output")
+ 
+const  getText =()=>{
+fetch("sample.txt")
+.then((res)=> res.text())
+.then((data)=>{
+return output.innerHTML = data;
+})
+.catch((err)=>{
+    console.log(err);
+})
 }
+
+
 getMyText.addEventListener("click", getText);
