47df65d55261cc061dd01d0d15a9447166c7e559
ciamac-da <ciamac.davoudi@gmail.com>
2020-12-13 13:49:16 +0100

    Verified by Cia v1.1.1 finally:)

--- addPost.js
+++ addPost.js
@@ -1,16 +1,19 @@
-const submitPost = document.getElementById("addPosts");
-let title = document.getElementsByTagName("title").value;
-let body = document.getElementsByTagName("body").value;
+const submitPost = document.getElementById("addPost");
 
 const submitMyPost = (e) =>{
-e.preventDefault();
-fetch("https://jsonplaceholder.typicode.com/posts", {
+    e.preventDefault();
+    
+    let title  = document.getElementById("title").value;
+    let body   = document.getElementById("body").value;
+    
+    fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "POST",
     headers: {
         "Accept":"application/json, text/plain, */*",
         "Content-Type":"application/json"
     },
     body:JSON.stringify({title:title, body:body})
+
 })
 .then((res)=> res.json())
 .then((data)=>console.log(data))
